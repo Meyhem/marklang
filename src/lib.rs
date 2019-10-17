d4582291b322012566c56ccd282ca777eb335593
Meyhem <meyhem@protonmail.com>
2019-10-17 19:53:57 +0200

    Using sparse fully, tests, pub interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 mod sparse;
 
+pub mod prelude {
+    pub use super::MarkovLanguageGenerator;
+}
+
 use rand::prelude::*;
 
 #[derive(Copy, Clone, Debug)]
@@ -8,10 +12,10 @@ struct ProbCell {
     count: usize
 }
 
-struct MarkovLanguageGenerator {
+pub struct MarkovLanguageGenerator {
     grams: Vec<String>,
     // matrix: sparse::SparseMatrix<String>,
-    mat: sparse::SparseMatrix<'a, ProbCell>,
+    mat: sparse::SparseMatrix<ProbCell>,
     rng: rand::rngs::ThreadRng,
     ngram: usize
 }
@@ -21,52 +25,60 @@ impl MarkovLanguageGenerator {
         return MarkovLanguageGenerator {
             // matrix: sparse::SparseMatrix::new(),
             grams: vec![],
-            mat: vec![vec![ProbCell{prob: 0.0, count: 0}; 0]; 0],
+            mat: sparse::SparseMatrix::new(),
             rng: rand::thread_rng(),
             ngram: ngram
         }
     }
+
     pub fn fit(&mut self, s: &String) -> Result<(), String> {
         let chars = s.chars().collect::<Vec<char>>();
         let n_chars = chars.iter().count();
-        if n_chars <= 2*self.ngram {
-            return Err("Text size must be longer than 2*ngram size".to_owned());
+        if n_chars < 2*self.ngram {
+            return Err("Text size must be at least 2*ngram size".to_owned());
         }
-        for i in 0..n_chars - self.ngram*2 {
+        for i in 0..n_chars - self.ngram * 2 {
             let gram1 = &chars[i..i + self.ngram];
             let gram2 = &chars[i + self.ngram..i + self.ngram * 2];
             let c1_i = self.get_or_insert_ngram_index(gram1.iter().collect());
             let c2_i = self.get_or_insert_ngram_index(gram2.iter().collect());
-            self.mat[c1_i][c2_i].count += 1;
+            match self.mat.get(c1_i, c2_i) {
+                Some(mut cell) => {
+                    cell.count += 1;
+                    self.mat.put(c1_i, c2_i, cell);
+                },
+                None => {
+                    self.mat.put(c1_i, c2_i, ProbCell { count: 1, prob: 0f32 });
+                }
+            }
         }
         self.adjust_probs();
         Ok(())
     }
+
     pub fn adjust_probs(&mut self) {
-        for row in self.mat.iter_mut() {
-            let row_total = row.iter().fold(0usize, |sum, &v| sum + v.count);
-            for col in row.iter_mut() {
+        for r in 0..self.grams.len() {
+            let row_total = self.mat.iter_row(r).fold(0usize, |sum, v| sum + v.count);
+            self.mat.row_for_each(r, |cell| {
                 if row_total == 0 {
-                    col.prob = 0f32;
+                    cell.prob = 0f32;
                 } else {
-                    col.prob = col.count as f32 / row_total as f32;
+                    cell.prob = cell.count as f32 / row_total as f32;
                 }
-            }
+            });
         }
     }
+
     pub fn get_or_insert_ngram_index(&mut self, c: String) -> usize {
         match self.grams.iter().position(|entry| *entry == c) {
             Some(i) => i,
             None => {
-                for row in self.mat.iter_mut() {
-                    row.push(ProbCell{prob: 0f32, count: 0});
-                }
-                self.mat.push(vec![ProbCell{prob: 0f32, count: 0}; self.mat.len() + 1]);
                 self.grams.push(c);
-                self.mat.len() - 1
+                self.grams.len() - 1
             }
         }
     }
+
     pub fn gen(&mut self, len: usize) -> Result<String, String> {
         let mut buf = String::with_capacity(len);
         let mut current = self.grams[self.rng.next_u64() as usize % self.grams.len()].clone();
@@ -76,7 +88,7 @@ impl MarkovLanguageGenerator {
             let mut cummulative_prob = 0f32;
             let mut index = 0;
             let row_i = self.get_or_insert_ngram_index(current);
-            for cell in self.mat[row_i].iter() {
+            for cell in self.mat.iter_row(row_i) {
                 cummulative_prob += cell.prob;
                 if cummulative_prob >= dest_prob {
                     break;
@@ -91,27 +103,16 @@ impl MarkovLanguageGenerator {
         }
         Ok(buf)
     }
-    pub fn print_count(&self) {
-        let ngram_space = " ".repeat(self.ngram);
-        print!("{} ", ngram_space);
-        for c in self.grams.iter() {
-            print!("{} ", c);
-        }
-        println!();
-        for (i, row) in self.mat.iter().enumerate() {
-            print!("{} ", self.grams[i]);
-            for col in row {
-                print!("{}{}", col.count, ngram_space);
-            }
-            println!();
-        }
-    }
 }
 
 
 #[cfg(test)]
 mod tests {
+    extern crate regex;
+
     use super::*;
+    use regex::Regex;
+    use std::str::FromStr;
 
     #[test]
     fn test_instantiation() {
@@ -122,6 +123,33 @@ mod tests {
     fn test_fit_ngram_len_validation() {
         let mut generator = MarkovLanguageGenerator::new(5);
 
-        assert_eq!(generator.fit(&"abc".to_owned()), Err("Text size must be longer than 2*ngram size".to_owned()));
+        assert_eq!(generator.fit(&"abc".to_owned()), Err("Text size must be at least 2*ngram size".to_owned()));
+    }
+
+    #[test]
+    fn test_larger_ngrams() {
+        let mut generator = MarkovLanguageGenerator::new(3);
+        generator.fit(&"helloo".to_owned()).unwrap();
+        generator.fit(&"hellooooo".to_owned()).unwrap();
+        generator.fit(&"hello".to_owned()).unwrap_err();
+    }
+
+    #[test]
+    fn test_fit() {
+        let mut generator = MarkovLanguageGenerator::new(1);
+        generator.fit(&"abbbbbc".to_owned()).unwrap();
+
+        assert_eq!(generator.mat.get(0, 1).unwrap().count, 1);
+        assert_eq!(generator.mat.get(1, 1).unwrap().count, 4);
+    }
+
+    #[test]
+    fn test_gen() {
+        let mut generator = MarkovLanguageGenerator::new(1);
+        let mut text: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan sit amet nulla facilisi morbi tempus. Risus in hendrerit gravida rutrum quisque non tellus orci. Commodo ullamcorper a lacus vestibulum sed arcu non. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Eu sem integer vitae justo. Nunc sed augue lacus viverra vitae congue eu. Mi proin sed libero enim sed faucibus turpis in eu. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Morbi tristique senectus et netus et malesuada fames ac turpis. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Tellus orci ac auctor augue mauris. Tellus id interdum velit laoreet id donec. Convallis a cras semper auctor. Eget gravida cum sociis natoque. Senectus et netus et malesuada fames ac turpis egestas integer.".to_owned();
+        text = text.to_lowercase();
+        let reg = Regex::from_str("[^A-Za-z]").unwrap();
+        text = reg.replace_all(&text, "").into_owned();
+        assert!(generator.fit(&text).is_ok());
     }
 }
--- src/sparse.rs
+++ src/sparse.rs
@@ -1,16 +1,17 @@
-struct Triple<'a, T> {
+#[derive(Debug)]
+struct Triple<T> {
     row: usize,
     col: usize,
-    value: &'a T
+    value: T
 }
 
-pub struct SparseMatrix<'a, T> {
-    triples: Vec<Triple<'a, T>>,
+pub struct SparseMatrix<T> {
+    triples: Vec<Triple<T>>,
     max_rows: usize,
     max_cols: usize
 }
 
-impl<'a, T> SparseMatrix<'a, T> {
+impl<T: Copy> SparseMatrix<T> {
     pub fn new() -> Self {
         SparseMatrix {
             triples: Vec::new(),
@@ -27,11 +28,11 @@ impl<'a, T> SparseMatrix<'a, T> {
         self.max_cols
     }
 
-    pub fn get(&self, row: usize, col: usize) -> Option<&T> {
+    pub fn get(&self, row: usize, col: usize) -> Option<T> {
         self.triples.iter().find(|t| t.row == row && t.col == col).map(|t| t.value)
     }
 
-    pub fn put(&mut self, row: usize, col: usize, value: &'a T) {
+    pub fn put(&mut self, row: usize, col: usize, value: T) {
         if self.max_rows < row {
             self.max_rows = row;
         }
@@ -46,16 +47,31 @@ impl<'a, T> SparseMatrix<'a, T> {
             },
             None => {
                 self.triples.push(Triple {row: row, col: col, value: value});
+                self.triples.sort_by(|a, b| a.row.cmp(&b.row));
             }
         }
     }
 
-    pub fn iter_row(&self, row: usize) -> impl Iterator<Item = &T> {
-        if row >= self.max_rows {
-            panic!("Row index out of range");
-        }
+    pub fn iter_row(&self, row: usize) -> impl Iterator<Item = T> + '_ {
+        self.triples
+            .iter()
+            .skip_while(move |t| t.row != row)
+            .take_while(move |t| t.row == row)
+            .map(|t| t.value)
+    }
 
-        self.triples.iter().filter(move |t| t.row == row).map(|t| t.value)
+    pub fn row_for_each<F>(&mut self, row: usize, mut f: F) where F: FnMut(&mut T)  {
+        for t in self.triples.iter_mut() {
+            if t.row < row {
+                continue;
+            }
+
+            if t.row > row {
+                break;
+            }
+
+            f(&mut t.value);
+        }
     }
 }
 
@@ -64,12 +80,27 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_row_for_each() {
+        #[derive(Clone, Copy, Debug, PartialEq)]
+        struct TestType { val: u32 }
+
+        let mut sm: SparseMatrix<TestType> = SparseMatrix::new();
+        sm.put(0, 0, TestType { val: 0 });
+        sm.put(0, 1, TestType { val: 1 });
+        sm.put(1, 0, TestType { val: 2 });
+        sm.put(1, 1, TestType { val: 3 });
+        sm.row_for_each(0, |v| v.val = 1000);
+
+        assert_eq!(sm.iter_row(0).collect::<Vec<TestType>>(), vec![TestType { val: 1000 }, TestType { val: 1000 }]);
+    }
+
+    #[test]
     fn test_put_get() {
         let mut sm: SparseMatrix<u32> = SparseMatrix::new();
-        sm.put(0, 0, &55);
-        assert_eq!(sm.get(0, 0), Some(&55));
-        sm.put(0, 0, &42);
-        assert_eq!(sm.get(0, 0), Some(&42));
+        sm.put(0, 0, 55);
+        assert_eq!(sm.get(0, 0), Some(55));
+        sm.put(0, 0, 42);
+        assert_eq!(sm.get(0, 0), Some(42));
     }
 
     #[test]
@@ -81,24 +112,16 @@ mod tests {
     #[test]
     fn test_row_iter() {
         let mut sm: SparseMatrix<u32> = SparseMatrix::new();
-        sm.put(0, 0, &0);
-        sm.put(1, 0, &1);
+        sm.put(0, 0, 0);
+        sm.put(1, 0, 1);
 
-        sm.put(0, 1, &0);
-        sm.put(1, 1, &1);
+        sm.put(0, 1, 0);
+        sm.put(1, 1, 1);
 
-        sm.put(0, 2, &2);
+        sm.put(0, 2, 2);
 
-        let row: Vec<&u32> = sm.iter_row(0).collect();
+        let row: Vec<u32> = sm.iter_row(0).collect();
 
-        assert_eq!(row, vec![&0, &0, &2]);
+        assert_eq!(row, vec![0, 0, 2]);
     }
-
-    #[test]
-    #[should_panic(expected = "Row index out of range")]
-    fn test_row_iter_panic() {
-        let sm: SparseMatrix<u32> = SparseMatrix::new();
-        sm.iter_row(42);
-    }
-
 }
